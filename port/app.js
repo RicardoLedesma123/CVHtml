8ba76941703e4bc5beb106591c1bb9e4cd9de438
Ricardo Ledesma <83382444+RicardoLedesma123@users.noreply.github.com>
2023-12-10 18:57:24 -0600

    Add files via upload

---
+++ port/app.js
@@ -0,0 +1,105 @@
+var menu_visible = false;
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu() {
+  if (menu_visible == false) {
+    //si esta oculto
+    menu.style.display = "block";
+    menu_visible = true;
+  } else {
+    menu.style.display = "none";
+    menu_visible = false;
+  }
+}
+//oculto el menu una vez que selecciono una opción
+let links = document.querySelectorAll("nav a");
+for (var x = 0; x < links.length; x++) {
+  links[x].onclick = function () {
+    menu.style.display = "none";
+    menu_visible = false;
+  };
+}
+
+//Creo las barritas de una barra particular identificada por su id
+function crearBarra(id_barra) {
+  for (i = 0; i <= 16; i++) {
+    let div = document.createElement("div");
+    div.className = "e";
+    id_barra.appendChild(div);
+  }
+}
+
+//selecciono todas las barras generales par aluego manipularlas
+let html = document.getElementById("html");
+crearBarra(html);
+let javascript = document.getElementById("javascript");
+crearBarra(javascript);
+let angular = document.getElementById("angular");
+crearBarra(angular);
+let cmas = document.getElementById("cmas");
+crearBarra(cmas);
+let react = document.getElementById("react");
+crearBarra(react);
+let hard = document.getElementById("hard");
+crearBarra(hard);
+let engl = document.getElementById("engl");
+crearBarra(engl);
+let bots = document.getElementById("bots");
+crearBarra(bots);
+
+//Ahora voy a guardar la cantidad de barritas que se van a ir pintando por cada barar
+//para eso utilizo un arreglo, cada posiciòn pertenece a un elemento
+//comienzan en -1 porque no tiene ninguna pintada al iniciarse
+let contadores = [-1, -1, -1, -1, -1, -1, -1, -1];
+//esta variable la voy a utilizar de bandera para saber si ya ejecuto la animación
+let entro = false;
+
+//función que aplica las animaciones de la habilidades
+function efectoHabilidades() {
+  var habilidades = document.getElementById("habilidades");
+  var distancia_skills =
+    window.innerHeight - habilidades.getBoundingClientRect().top;
+  if (distancia_skills >= 300 && entro == false) {
+    entro = true;
+    const intervalHtml = setInterval(function () {
+      pintarBarra(html, 13, 0, intervalHtml);
+    }, 100);
+    const intervalJavascript = setInterval(function () {
+      pintarBarra(javascript, 10, 1, intervalJavascript);
+    }, 100);
+    const intervalAngular = setInterval(function () {
+      pintarBarra(angular, 11, 2, intervalAngular);
+    }, 100);
+    const intervalCmas = setInterval(function () {
+      pintarBarra(cmas, 13, 3, intervalCmas);
+    }, 100);
+    const intervalReact = setInterval(function () {
+      pintarBarra(react, 5, 4, intervalReact);
+    }, 100);
+    const intervalHard = setInterval(function () {
+      pintarBarra(hard, 13, 5, intervalHard);
+    }, 100);
+    const intervalEngl = setInterval(function () {
+      pintarBarra(engl, 16, 6, intervalEngl);
+    }, 100);
+    const intervalBots = setInterval(function () {
+      pintarBarra(bots, 14, 7, intervalBots);
+    }, 100);
+  }
+}
+
+//lleno una barra particular con la cantidad indicada
+function pintarBarra(id_barra, cantidad, indice, interval) {
+  contadores[indice]++;
+  x = contadores[indice];
+  if (x < cantidad) {
+    let elementos = id_barra.getElementsByClassName("e");
+    elementos[x].style.backgroundColor = "#940253";
+  } else {
+    clearInterval(interval);
+  }
+}
+
+//detecto el scrolling del mouse para aplicar la animación de la barra
+window.onscroll = function () {
+  efectoHabilidades();
+};
